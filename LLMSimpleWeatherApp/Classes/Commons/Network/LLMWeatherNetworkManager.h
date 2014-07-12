ddf538703697ba705c81bf2a3b7036c7290689e7
Manuele Mion <manuele.mion@callsign.com>
2014-07-12 09:16:01 +0100

    Added the API key to the network class

--- LLMSimpleWeatherApp/Classes/Commons/Network/LLMWeatherNetworkManager.h
+++ LLMSimpleWeatherApp/Classes/Commons/Network/LLMWeatherNetworkManager.h
@@ -9,4 +9,13 @@
 
 @interface LLMWeatherNetworkManager : NSObject <LLMWeatherDataManagerInterface>
 
+/*
+ * This is the designated init for the class. The other init method should be used.
+ *
+ * APIKey: key used by the class to exchange data with the API server
+ *
+ * returns: A LLWeatherNetworkManager instance, nil if there is any problem
+ */
+- (id)initWithAPIKey:(NSString *)apiKey;
+
 @end
--- LLMSimpleWeatherApp/Classes/Commons/Network/LLMWeatherNetworkManager.m
+++ LLMSimpleWeatherApp/Classes/Commons/Network/LLMWeatherNetworkManager.m
@@ -10,14 +10,36 @@
 // TODO: Move the base URL to the init, use the path to call the proper api method
 NSString * const LLMWeatherNetworkManagerBaseURLString = @"http://api.openweathermap.org/data/2.5/weather";
 
+@interface LLMWeatherNetworkManager ()
+
+@property (nonatomic, strong) NSString *apiKey;
+
+@end
+
 @implementation LLMWeatherNetworkManager
+
+// The default init is overwritten to return an exception if used since we want to force the class client
+// to give us the apiKey.
+- (id)init {
+    NSAssert(NO, @"Method 'initWithAPIKey' should be used instead of this.");
+    return nil;
+}
+
+- (id)initWithAPIKey:(NSString *)apiKey {
+
+    self = [super init];
+    if (self) {
+        self.apiKey = apiKey;
+    }
+    return self;
+}
 
 - (void)getWeatherForecastForDate:(NSDate *)date
                            cityOf:(NSString *)cityName
                           success:(LLMWeatherNetworkManagerSuccess)successBlock
                             error:(LLMWeatherNetworkManagerFailure)errorBlock {
 
-    NSDictionary *parameters = @{@"q": cityName};
+    NSDictionary *parameters = @{@"q": cityName, @"APPID": self.apiKey};
     AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
     [manager GET:LLMWeatherNetworkManagerBaseURLString parameters:parameters
     success:^(AFHTTPRequestOperation *operation, id responseObject) {
